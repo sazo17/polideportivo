e950d941eebd59cdc603d155596e3d112ad258ff
srjefers <ajefersonstiv@gmail.com>
2018-07-31 08:59:38 -0600

    Cambio ER se agrego la tabla `registro_arbitro_jornada` Se mejoro la relaciones entre la tabla `arbitros`y la tabla `jornada`.

--- Scripts_BD/f_plLogin.sql
+++ Scripts_BD/f_plLogin.sql
@@ -1,37 +1,38 @@
 DELIMITER //
-CREATE PROCEDURE p_login(IN user VARCHAR(16), 
-IN pass VARCHAR(30),
-OUT o_tipo_usuarios VARCHAR(1), 
-OUT o_estado_usuarios VARCHAR(1),
-OUT o_idUsuarios INT(11)) 
+CREATE PROCEDURE p_login(IN user VARCHAR(16)
+, IN pass VARCHAR(30)
+, OUT o_tipo_usuarios VARCHAR(1) 
+, OUT o_estado_usuarios VARCHAR(1)
+, OUT o_idUsuarios INT(11)
+) 
 BEGIN
 	DECLARE s_tipo,  s_estado VARCHAR(1);
-    DECLARE s_id INT(11);
+   DECLARE s_id INT(11);
     
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
-		SET o_tipo_usuarios = 0, o_estado_usuarios = 0, o_idUsuarios = 0;
+   DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
+	SET o_tipo_usuarios = 0, o_estado_usuarios = 0, o_idUsuarios = 0;
         
 	SELECT Tipo_usuarios, estado_usuarios, idUsuarios
 	INTO s_tipo, s_estado, s_id
-    FROM USUARIOS
+	FROM USUARIOS
 	WHERE 
-		user = user_usuarios AND
-        pass = pass_usuarios;
+		user_usuarios = user AND
+      pass_usuarios = pass;
         -- SELECT s_id, s_estado, s_tipo;
         
-    IF s_id IS NOT NULL THEN
+   IF s_id IS NOT NULL THEN
 		SET o_tipo_usuarios = s_tipo;
-        SET o_estado_usuarios = s_estado;
-        SET o_idUsuarios = s_id;
+      SET o_estado_usuarios = s_estado;
+      SET o_idUsuarios = s_id;
         
 		INSERT INTO BITACORA_LOGIN (usuario_bitacora, 
-        fecha_bitacora, host_bitacora) 
-        VALUES (s_id, SYSDATE(), USER());
+      fecha_bitacora, host_bitacora) 
+      VALUES (s_id, SYSDATE(), USER());
 		-- COMMIT;
 	ELSE 
 		SET o_tipo_usuarios = 'N';
-        SET o_estado_usuarios = 'N';
-        SET o_idUsuarios = 0;
+      SET o_estado_usuarios = 'N';
+      SET o_idUsuarios = 0;
     
     END IF;
 END//
