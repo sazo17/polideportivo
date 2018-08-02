3c81401e82da1b18a43c976ec1a90d0d340ac205
srjefers <ajefersonstiv@gmail.com>
2018-08-01 18:38:30 -0600

    Procedimientos modificados Procedimientos en fase Beta Los procedimientos 	* p_campeonato 	* p_jug_registro Han sido modificados y adapatados a la ultima version del diagrama ER. Funcionan correctamente. Futuras validaciones seran agregadas.

--- Scripts_BD/procedimientos.sql
+++ Scripts_BD/procedimientos.sql
@@ -3,7 +3,7 @@
 ALTER TABLE BITACORA_LOGIN MODIFY fecha_bitacora TIMESTAMP;
 
 -- Codigo PL/MySQL 
-DELIMITER //
+DELIMITER //									-- Funciona correctamente
 -- Definicion de cada uno de los parametros IN parametros que recibe
 -- OUT salidas a la aplicacion.
 CREATE PROCEDURE p_login(IN user VARCHAR(16)
@@ -59,187 +59,136 @@ SELECT @a, @b, @c;
 SELECT * FROM BITACORA_LOGIN;
 
 
-
--- -------------------------------
+-- ------------------------------------
 -- Procedimiento para campeonato
 -- Insertando datos en tabla transaccional `p_campeonato`
-DELIMITER //
+DELIMITER //									-- Fase BETA
 -- Definiendo parametros en `p_campeonato`
-CREATE PROCEDURE p_campeonato(IN nombre VARCHAR(50),
-IN fecha_ini DATE, IN fecha_fin DATE, IN cantidad INT,
-IN id_empleado INT, IN tipo_camp VARCHAR(30), IN deporte VARCHAR(35),
-OUT bit INT(1))
+CREATE PROCEDURE p_campeonato(IN i_nombre VARCHAR(50),
+IN i_fecha_ini DATE, IN i_fecha_fin DATE, IN i_cantidad INT,
+IN id_empleado INT, IN tipo_camp VARCHAR(30), IN i_deporte INT(11),
+OUT o_bit INT(1))
 BEGIN
 	-- Declaracion de variables
 	DECLARE t_tipo_camp INT;
-	DECLARE t_deporte INT;
-	DECLARE deporte_id INT;
+    DECLARE t_deporte INT;
+    DECLARE tDeporte_id INT;
 	
 	-- Se asignan los valores encontrados a las variables
 	SELECT idTipo_campeonato INTO t_tipo_camp 
 	FROM TIPO_CAMPEONATO
 	WHERE Tipo_Tipo_campeonato = tipo_camp;
 	-- ----------
-	SELECT idTipo_Deporte INTO t_deporte
-	FROM TIPOS_DEPORTE
-	WHERE tipo_Tipo_Deporte = deporte;
+     SELECT idTipo_Deporte INTO tDeporte_id
+     FROM DEPORTE
+     WHERE idDeporte = i_deporte;
+	-- SELECT idTipo_Deporte INTO t_deporte
+	-- FROM TIPOS_DEPORTE
+	-- WHERE tipo_Tipo_Deporte = i_deporte;
 	--
-	SELECT idDeporte INTO deporte_id
-	FROM DEPORTE
-	WHERE idTipo_Deporte = t_deporte;
+	-- SELECT idDeporte INTO deporte_id
+	-- FROM DEPORTE
+	-- WHERE idTipo_Deporte = t_deporte;
 
 	-- Validacion de las variables 
-	IF deporte_id IS NOT NULL AND t_tipo_camp IS NOT NULL THEN
+	IF tDeporte_id IS NOT NULL AND t_tipo_camp IS NOT NULL THEN
 		INSERT INTO CAMPEONATO
 		(nombre_campeonato, fecha_inicio_campenato,
 		fecha_finalización_campeonato, cantidad_equipos_campeonato,
 		idEmpleados, idTipo_campeonato, idDeporte, idTipo_Deporte)
 		VALUES (
-			nombre, fecha_ini, fecha_fin, cantidad, id_empleado,
-			t_tipo_camp, deporte_id, t_deporte);
+			i_nombre, i_fecha_ini, i_fecha_fin, i_cantidad, id_empleado,
+			t_tipo_camp, tDeporte_id, i_deporte);
 		-- Retorna 1 si el procedimiento se desarrollo normalmente.
-		SET bit = 1;
+		SET o_bit = 1;
         COMMIT;
 	ELSE 
 		-- Retorna 0 si las variables estaban NULL
-		SET BIT = 0;
+		SET o_bit = 0;
         ROLLBACK;
 	END IF;
 END//
 DELIMITER ;
 
 -- Solo para pruebas
--- CALL p_campeonato('campeonato12', '2018-08-09', '2017-08-19', 25, 35, 'tipo_c11', 'tipo1',@a);
+-- CALL p_campeonato('campeonato1', '2018-08-09', '2017-08-19', 25, 2, 'tipoc_1', 2,@a);
 -- SELECT @a;
 
 
-
+-- ---------------------------------
 -- Procedimiento registro_jugadores
-DELIMITER //
+DELIMITER //																	-- Fase BETA
 -- Definicion de parametros en `p_jug_registro`
-CREATE PROCEDURE p_jug_registro(IN fecha_ini DATE, IN fecha_fin DATE,
-IN desc_jugreg VARCHAR(50), IN equipo VARCHAR(35), IN id_jugador INT,
-IN posjug INT, OUT bit INT(1))
+CREATE PROCEDURE p_jug_registro(IN i_fecha_ini DATE, IN i_fecha_fin DATE,
+IN i_desc_jugreg VARCHAR(50), IN i_equipo VARCHAR(35), IN i_id_jugador INT,
+IN i_posjug INT, OUT o_bit INT(1))
 BEGIN
 	-- Declaracion de variables
 	DECLARE t_equipo INT;
-	DECLARE t_posjug INT;
 	
-	-- Asignacion de valores encontrados
+    -- Asignacion de valores encontrados
 	SELECT idEquipos INTO t_equipo
 	FROM EQUIPOS 
-	WHERE nombre_equipo = equipo;
+	WHERE nombre_equipo = i_equipo;
 	-- -----
-	SELECT id_pogjug INTO t_posjug
-	FROM POSICION_JUGADOR
-	WHERE posicion_posicionjugador = posjug;
+	-- SELECT id_pogjug INTO t_posjug
+	-- FROM POSICION_JUGADOR
+	-- WHERE posicion_posicionjugador = i_posjug;
 	
-	-- Validacion de variables != NULL
-	IF t_equipo IS NOT NULL AND t_posjug IS NOT NULL THEN
+    -- Validacion de variables != NULL
+	IF t_equipo IS NOT NULL THEN
 		INSERT INTO JUGADORES_REGISTRO 
 		(fecha_ini_jugadores_registro, fecha_fin_jugadores_registro, 
 		descripcion_jugadoresregistro, idEquipos, idJugadores,
-		idpogjug) 
-		VALUES (fecha_ini, fecha_fin, desc_jugreg, t_equipo, id_jugador,
-			t_posjug);
+		id_pogjug) 
+		VALUES (i_fecha_ini, i_fecha_fin, i_desc_jugreg, 
+        t_equipo, i_id_jugador,
+		i_posjug);
 		
 		-- Retorna 1 si todo se realizo correctamente
-		SET bit = 1;
+		SET o_bit = 1;
 		COMMIT;
 	ELSE 
-		
 		-- Retorna 0 si no se realizo correctamente
-		SET bit = 0;
+		SET o_bit = 0;
 		ROLLBACK;
 	END IF;
 END//
 DELIMITER ;
 
--- CALL p_jug_registro('2018-01-05', '2019-12-02', '884 94 AKIW', 'Jabbercube', 159, @a);
+-- Ejecutando PL/MySQL para pruebas
+-- CALL p_jug_registro('2018-01-05', '2019-12-02', '884 94 AKIW', 'equipo1', 105, 1,@a);
 -- SELECT @a; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 -- AUN EN DESARROLLO Y PRUEBAS 
--- Procedimiento para campeonato
-DELIMITER //
-CREATE PROCEDURE p_campeonato(IN nombre VARCHAR(50),
-IN fecha_ini DATE, IN fecha_fin DATE, IN cantidad INT,
-IN id_empleado INT, IN tipo_camp VARCHAR(30), IN deporte VARCHAR(35),
-OUT bit INT(1))
-BEGIN
-	DECLARE t_tipo_camp INT;
-	DECLARE t_deporte INT;
-	DECLARE deporte_id INT;
-	
-	SELECT idTipo_campeonato INTO t_tipo_camp 
-	FROM TIPO_CAMPEONATO
-	WHERE Tipo_Tipo_campeonato = tipo_camp;
-	-- ----------
-	SELECT idTipo_Deporte INTO t_deporte
-	FROM TIPOS_DEPORTE
-	WHERE tipo_Tipo_Deporte = deporte;
-	--
-	SELECT idDeporte INTO deporte_id
-	FROM DEPORTE
-	WHERE idTipo_Deporte = t_deporte;
-	IF deporte_id IS NOT NULL AND t_tipo_camp IS NOT NULL THEN
-		INSERT INTO CAMPEONATO
-		(nombre_campeonato, fecha_inicio_campenato,
-		fecha_finalización_campeonato, cantidad_equipos_campeonato,
-		idEmpleados, idTipo_campeonato, idDeporte, idTipo_Deporte)
-		VALUES (
-			nombre, fecha_ini, fecha_fin, cantidad, id_empleado,
-			t_tipo_camp, deporte_id, t_deporte);
-		SET BIT = 1;
-        COMMIT;
-	ELSE 
-		SET BIT = 0;
-        ROLLBACK;
-	END IF;
-END//
-DELIMITER ;
 
--- CALL p_campeonato('campeonato12', '2018-08-09', '2017-08-19', 25, 35, 'tipo_c11', 'tipo1',@a);
--- SELECT @a;
-
--- Procedimiento registro_jugadores
-DELIMITER //
-CREATE PROCEDURE p_jug_registro(IN fecha_ini DATE, IN fecha_fin DATE,
-IN desc_jugreg VARCHAR(50), IN equipo VARCHAR(35), IN id_jugador INT,
-IN posjug INT, OUT bit INT(1))
-BEGIN
-	DECLARE t_equipo INT;
-	DECLARE t_posjug INT;
-	
-	SELECT idEquipos INTO t_equipo
-	FROM EQUIPOS 
-	WHERE nombre_equipo = equipo;
-	-- -----
-	SELECT id_pogjug INTO t_posjug
-	FROM POSICION_JUGADOR
-	WHERE posicion_posicionjugador = posjug;
-	
-	IF t_equipo IS NOT NULL AND t_posjug IS NOT NULL THEN
-		INSERT INTO JUGADORES_REGISTRO 
-		(fecha_ini_jugadores_registro, fecha_fin_jugadores_registro, 
-		descripcion_jugadoresregistro, idEquipos, idJugadores,
-		idpogjug) 
-		VALUES (fecha_ini, fecha_fin, desc_jugreg, t_equipo, id_jugador,
-			t_posjug);
-		SET bit = 1;
-		COMMIT;
-	ELSE 
-		SET bit = 0;
-		ROLLBACK;
-	END IF;
-END//
-DELIMITER ;
-
-
--- DROP PROCEDURE p_jug_registro;
--- CALL p_jug_registro('2018-01-05', '2019-12-02', '884 94 AKIW', 'Jabbercube', 159, @a);
--- SELECT @a; 
 
 DROP PROCEDURE p_jornada;
 USE p_polideportivo;
