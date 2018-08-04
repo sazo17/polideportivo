fe85f3db84128887f32bd462200bf4618d9ad78e
Pablo Moises Mazariegos Pinto <pablomazariegos15@gmail.com>
2018-08-04 10:43:52 -0600

    actualizando conexion

--- WpfApp1/WpfApp1/conexion_mysql.cs
+++ WpfApp1/WpfApp1/conexion_mysql.cs
@@ -16,8 +16,8 @@ namespace WpfApp1
         public static MySqlConnection con_mysql = null;       // variable de conexion
         public const String servidor = "localhost";    // ubicacion|ip del servidor
         public const String base_datos = "p_polideportivo";     // id|nombre de la base de datos
-        public const String usuario = "rot";              // usuario dentro de la base de datos
-        public const String pass = "";                 // contrasenia del usuario
+        public const String usuario = "usuario";              // usuario dentro de la base de datos
+        public const String pass = "1234";                 // contrasenia del usuario
 
 
 
