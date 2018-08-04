9343ae619c2c1e7cb8705992a8ebaecb80b1a1f5
srjefers <ajefersonstiv@gmail.com>
2018-08-04 16:20:34 -0600

    Resolucion de antigua version publicada equivocamente Se resuelve los errores encontrados en la version `p_Login` antigua El archivo `p_Login` habia sido eliminado durante la restructuracion de los procediemientos, se agrega un nuevo archivo el cual contiene unicamente ese procedimiento.

--- Scripts_BD/p_loginF.sql
+++ Scripts_BD/p_loginF.sql
@@ -1,55 +1,48 @@
--- Es necesario realizar esta modificacion en el campo de la tabla
--- Workbench por si solo no permitia definir el tipo de dato `TIMESTAMP`
-ALTER TABLE BITACORA_LOGIN MODIFY fecha_bitacora TIMESTAMP;
+-- Dependencia SINO FUNCIONA ES PORQUE YA HA SIDO APLICADA
+ALTER TABLE USUARIOS ADD CONSTRAINT cu_user UNIQUE(user_usuarios);
 
--- Codigo PL/MySQL 
+
 DELIMITER //
-CREATE PROCEDURE p_login(IN user VARCHAR(16)
+CREATE  PROCEDURE `p_login`(IN user VARCHAR(16)
 , IN pass VARCHAR(30)
 , OUT o_tipo_usuarios VARCHAR(1) 
 , OUT o_estado_usuarios VARCHAR(1)
-, OUT o_idUsuarios INT(11)
-) 
+, OUT o_idUsuarios INT(11))
 BEGIN
+	-- Declaracion de variables
 	DECLARE s_tipo,  s_estado VARCHAR(1);
 	DECLARE s_id INT(11);
-    
+    	-- Declaracion de Exception
 	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
 	SET o_tipo_usuarios = 0, o_estado_usuarios = 0, o_idUsuarios = 0;
         
+	-- Busqueda de usuario y contrasenia.
+	-- Se asigna en una variable cuando se encuentra.
 	SELECT Tipo_usuarios, estado_usuarios, idUsuarios
 	INTO s_tipo, s_estado, s_id
 	FROM USUARIOS
 	WHERE 
 		user_usuarios = user AND
 		pass_usuarios = pass;
-        -- SELECT s_id, s_estado, s_tipo;
         
+	-- Validacion de `s_id` si no es NULL puede proceder
 	IF s_id IS NOT NULL THEN
+		-- Asignacion de los valores recibidos en las variables
+		-- de salida.
 		SET o_tipo_usuarios = s_tipo;
 		SET o_estado_usuarios = s_estado;
 		SET o_idUsuarios = s_id;
-        
+        	-- Inserta datos en la `bitacora_login` 
+		-- inserta la fecha actual + hora y el host = hostname@puerto
 		INSERT INTO BITACORA_LOGIN (usuario_bitacora, 
 		fecha_bitacora, host_bitacora) 
 		VALUES (s_id, SYSDATE(), USER());
 		COMMIT;
 	ELSE 
+		-- Retonra valores asignados en variables cuando `s_id`es NULL
 		SET o_tipo_usuarios = 'N';
 		SET o_estado_usuarios = 'N';
 		SET o_idUsuarios = 0;				  
     END IF;
 END//
 DELIMITER ;
-
--- Ejecutando PL/MySQL para pruebas
-CALL p_login('usuario1', '1234', @a, @b, @c);
-SELECT @a, @b, @c;
--- Verificando tabla `Bitacora_Login`
-SELECT * FROM BITACORA_LOGIN;
-
-
-
--- USE p_polideportivo;
--- DROP PROCEDURE p_login;
--- DESC BITACORA_LOGIN;
